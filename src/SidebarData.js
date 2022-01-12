c4a7908a163776d4112486bf9aa8adeca292b81d
sarnakhmel <OlSa@MacBook-Pro-username.local>
2022-01-12 15:55:46 +0200

    NavBar and style

--- src/SidebarData.js
+++ src/SidebarData.js
@@ -1,6 +1,25 @@
 import React from "react";
-import * as FaIcons from "react-icons/fa";
+//import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
+//import * as IoIcons from "react-icons/io";
 
-//export const
+export const SideBar = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Education",
+    path: "/education",
+    icon: <AiIcons.AiFillBook />,
+    cName: "nav-text",
+  },
+  {
+    title: "Work",
+    path: "/work",
+    icon: <AiIcons.MdWork />,
+    cName: "nav-text",
+  },
+];
